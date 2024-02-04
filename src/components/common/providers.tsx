435facbebeaa64c880a7898e60c0bbb295ef6657
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-04 14:20:10 +0330

    feat: add mantine theme

--- src/components/common/providers.tsx
+++ src/components/common/providers.tsx
@@ -3,10 +3,12 @@ import '@mantine/core/styles.css'
 import {MantineProvider} from '@mantine/core'
 import type {ReactNode} from 'react'
 
+import {theme} from '@/lib/theme'
+
 interface ProvidersProps {
   children: ReactNode
 }
 
 export function Providers({children}: ProvidersProps) {
-  return <MantineProvider>{children}</MantineProvider>
+  return <MantineProvider theme={theme}> {children}</MantineProvider>
 }
