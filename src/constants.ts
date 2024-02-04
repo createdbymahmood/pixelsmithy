66b9410c4f66f15715b9393d5318ac7e877c307e
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-04 14:34:43 +0330

    feat: add @vanilla-extract

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+import type {MantineColorScheme} from '@mantine/core'
+
+export const defaultThemeColorScheme: MantineColorScheme = 'light'
--- src/lib/theme.ts
+++
@@ -1,3 +0,0 @@
-import {createTheme} from '@mantine/core'
-
-export const theme = createTheme({})
