e95f92fc6043c6f43c1a0f495f1cb3bf97db734e
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-04 14:13:05 +0330

    feat: setup Mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,33 @@
+import {ColorSchemeScript} from '@mantine/core'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
-import Head from 'next/head'
+
+import {Providers} from '@/components/common/providers'
+import {constructMetadata} from '@/utils/constructMetadata'
 
 const inter = Inter({subsets: ['latin']})
 
-export const metadata: Metadata = {
+export const metadata: Metadata = constructMetadata({
   title: 'Pixelsmithy',
+})
+
+interface RootLayoutProps {
+  readonly children: React.ReactNode
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function RootLayout({children}: RootLayoutProps) {
   return (
     <html lang='en'>
-      <Head>
+      <head>
         <meta
           content='minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no'
           name='viewport'
         />
-      </Head>
-      <body className={inter.className}>{children}</body>
+        <ColorSchemeScript defaultColorScheme='light' />
+      </head>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
