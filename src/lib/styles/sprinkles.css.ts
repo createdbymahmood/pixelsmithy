b72a520fc25aff661707ba749096a6a4b9e41f31
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-05 15:54:11 +0330

    fix: CSS errors from VanillaExtract

--- src/lib/styles/sprinkles.css.ts
+++ src/lib/styles/sprinkles.css.ts
@@ -1,6 +1,6 @@
 import {createSprinkles} from '@vanilla-extract/sprinkles'
 
-import {colorProperties} from '@/lib/styles/color'
-import {responsiveProperties} from '@/lib/styles/responsiveProperties'
+import {colorProperties} from '@/lib/styles/color.css'
+import {responsiveProperties} from '@/lib/styles/responsiveProperties.css'
 
 export const sprinkles = createSprinkles(responsiveProperties, colorProperties)
