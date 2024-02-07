093c23a5f986eeaf34c03ad02486d03eb6c35148
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-07 13:29:53 +0330

    feat: add JobsSiderbar

--- src/components/JobLocation/JobLocation.tsx
+++ src/components/JobLocation/JobLocation.tsx
@@ -1,22 +1,28 @@
 'use client'
 
 import {Container, Grid} from '@mantine/core'
+import {Fragment} from 'react'
 
 import {JobFilters} from '@/components/JobLocation/JobFilters/JobFilters'
 import {JobsList} from '@/components/JobLocation/JobsList'
+import {JobsSiderbar} from '@/components/JobLocation/JobsSidebar/JobsSiderbar'
 
 export function JobLocation() {
   return (
-    <Container maw={1920} my='sm'>
-      <Grid gutter='xl'>
-        <Grid.Col maw={280} span='auto' w={280}>
-          <JobFilters />
-        </Grid.Col>
+    <Fragment>
+      <JobsSiderbar />
 
-        <Grid.Col span={{lg: 'auto'}}>
-          <JobsList />
-        </Grid.Col>
-      </Grid>
-    </Container>
+      <Container maw={1920} my='xl'>
+        <Grid gutter='xl'>
+          <Grid.Col maw={280} offset={{xs: 0, lg: 1}} span='auto' w={280}>
+            <JobFilters />
+          </Grid.Col>
+
+          <Grid.Col span={{lg: 'auto'}}>
+            <JobsList />
+          </Grid.Col>
+        </Grid>
+      </Container>
+    </Fragment>
   )
 }
