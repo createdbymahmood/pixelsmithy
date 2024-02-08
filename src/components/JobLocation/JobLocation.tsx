1c6b53092fce47573fb61dc9b903aa91bb29ec13
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-08 13:17:52 +0330

    refactor: a little bit of separation of components

--- src/components/JobLocation/JobLocation.tsx
+++ src/components/JobLocation/JobLocation.tsx
@@ -8,24 +8,29 @@ import {JobsList} from '@/components/JobLocation/JobsList'
 import {JobsSiderbar} from '@/components/JobLocation/JobsSidebar/JobsSiderbar'
 import {useMobileDevice} from '@/hooks/useMobileDevice'
 
+function JobLocationContent() {
+  return (
+    <Container maw={rem(1920)} my='lg'>
+      <Grid gutter='xl'>
+        <Grid.Col maw={rem(280)} offset={{xs: 0, lg: 1}} span='auto' w={280}>
+          <JobFilters />
+        </Grid.Col>
+
+        <Grid.Col span={{lg: 'auto'}}>
+          <JobsList />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
+}
+
 export function JobLocation() {
   const isMobile = useMobileDevice()
   return (
     <Box pt={isMobile ? 0 : rem(80)}>
       <JobsSiderbar />
       <JobLocationHeader />
-
-      <Container maw={rem(1920)} my='lg'>
-        <Grid gutter='xl'>
-          <Grid.Col maw={rem(280)} offset={{xs: 0, lg: 1}} span='auto' w={280}>
-            <JobFilters />
-          </Grid.Col>
-
-          <Grid.Col span={{lg: 'auto'}}>
-            <JobsList />
-          </Grid.Col>
-        </Grid>
-      </Container>
+      <JobLocationContent />
     </Box>
   )
 }
