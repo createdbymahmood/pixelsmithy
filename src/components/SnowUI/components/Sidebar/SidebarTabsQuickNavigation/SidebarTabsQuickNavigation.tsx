b35504077550b50fb57f5e6455cf1a857ccc82d8
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-10 14:44:10 +0330

    feat: add SidebarTabsQuickNavigation

---
+++ src/components/SnowUI/components/Sidebar/SidebarTabsQuickNavigation/SidebarTabsQuickNavigation.tsx
@@ -0,0 +1,71 @@
+import {Group, List, Tabs} from '@mantine/core'
+import {head} from 'lodash-es'
+
+import styles from './SidebarTabsQuickNavigation.module.scss'
+
+const tabs = [
+  {
+    key: 'favorites',
+    title: 'Favorites',
+    items: [
+      {key: '1', title: 'Microsoft', href: '/'},
+      {key: '2', title: 'Google', href: '/'},
+    ],
+  },
+
+  {
+    key: 'recently',
+    title: 'Recently',
+    items: [
+      {key: '1', title: 'Netflix', href: '/'},
+      {key: '2', title: 'Facebook', href: '/'},
+    ],
+  },
+]
+
+const initialOpenedTab = head(tabs)?.key
+
+export function SidebarTabsQuickNavigation() {
+  return (
+    <Tabs
+      classNames={{
+        tab: styles.tab,
+        tabSection: styles.tabSection,
+      }}
+      defaultValue={initialOpenedTab}
+      unstyled
+    >
+      <Tabs.List>
+        <Group>
+          {tabs.map((tab) => (
+            <Tabs.Tab key={tab.key} value={tab.key}>
+              {tab.title}
+            </Tabs.Tab>
+          ))}
+        </Group>
+      </Tabs.List>
+
+      {tabs.map((tab) => {
+        return (
+          <Tabs.Panel
+            key={tab.key}
+            classNames={{panel: styles.panel}}
+            mt='lg'
+            pl='lg'
+            value={tab.key}
+          >
+            <List
+              classNames={{item: styles.item}}
+              listStyleType='none'
+              spacing='md'
+            >
+              {tab.items.map((tabItem) => {
+                return <List.Item key={tabItem.key}>{tabItem.title}</List.Item>
+              })}
+            </List>
+          </Tabs.Panel>
+        )
+      })}
+    </Tabs>
+  )
+}
