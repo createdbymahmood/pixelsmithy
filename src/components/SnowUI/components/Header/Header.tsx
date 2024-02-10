2b5b9cafe3592425c66897a1b9aa48849c206df7
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-10 20:13:28 +0330

    refactor: some enhancement in dashboard layout

--- src/components/SnowUI/components/Header/Header.tsx
+++ src/components/SnowUI/components/Header/Header.tsx
@@ -1,7 +1,33 @@
-import {Group} from '@mantine/core'
+import {Anchor, Breadcrumbs, Group} from '@mantine/core'
 
 import styles from './Header.module.scss'
 
+const breadcrumbs = [
+  {title: 'Dashboards', href: '/'},
+  {title: 'Default', href: '/'},
+].map((item, index) => (
+  <Anchor key={index} href={item.href} underline='never'>
+    {item.title}
+  </Anchor>
+))
+
+const HeaderBreadcrumbs = () => {
+  return (
+    <Breadcrumbs
+      classNames={{
+        breadcrumb: styles.breadcrumb,
+        separator: styles.breadcrumbSeparator,
+      }}
+    >
+      {breadcrumbs}
+    </Breadcrumbs>
+  )
+}
+
 export function Header() {
-  return <Group className={styles.header} />
+  return (
+    <Group className={styles.header}>
+      <HeaderBreadcrumbs />
+    </Group>
+  )
 }
