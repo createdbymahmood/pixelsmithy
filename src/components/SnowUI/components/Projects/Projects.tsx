eb89c735258f7eb0b6d34f08f8536eb79e855c3d
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-12 21:14:09 +0330

    feat: add projects overview cards

---
+++ src/components/SnowUI/components/Projects/Projects.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import {Group, SimpleGrid, Stack, Text, Title} from '@mantine/core'
+import type {IconProps} from '@phosphor-icons/react'
+import {
+  CurrencyCircleDollar,
+  Folder,
+  TrendDown,
+  TrendUp,
+  UsersThree,
+} from '@phosphor-icons/react'
+import React from 'react'
+
+import styles from './Projects.module.scss'
+
+interface ProjectOverviewCardProps {
+  title: string
+  insight: string
+  value: string
+  icon: React.FC<IconProps>
+  valueIcon: React.FC<IconProps>
+}
+
+function ProjectOverviewCard({
+  icon: Icon,
+  insight,
+  title,
+  value,
+  valueIcon: ValueIcon,
+}: ProjectOverviewCardProps) {
+  return (
+    <Stack className={styles.card}>
+      <Group justify='space-between'>
+        <Text className={styles.cardTitle} size='sm'>
+          {title}
+        </Text>
+        <Icon size={24} />
+      </Group>
+
+      <Group justify='space-between'>
+        <Title order={3}>{insight}</Title>
+        <Group gap='xs'>
+          <Text size='sm'>{value}</Text>
+          <ValueIcon size={16} weight='fill' />
+        </Group>
+      </Group>
+    </Stack>
+  )
+}
+
+const overviews: ProjectOverviewCardProps[] = [
+  {
+    title: 'Current Projects',
+    icon: Folder,
+    insight: '268',
+    value: '+11.02%',
+    valueIcon: TrendUp,
+  },
+  {
+    title: 'Project Finance',
+    icon: CurrencyCircleDollar,
+    insight: '$3,290',
+    value: '+11.02%',
+    valueIcon: TrendDown,
+  },
+  {
+    title: 'Our clients',
+    icon: UsersThree,
+    insight: '31',
+    value: '+15.03%',
+    valueIcon: TrendUp,
+  },
+]
+
+function ProjectsOverview() {
+  const content = overviews.map((overview) => {
+    return <ProjectOverviewCard key={overview.title} {...overview} />
+  })
+  return <SimpleGrid cols={{xl: 3, lg: 2, md: 2}}>{content}</SimpleGrid>
+}
+
+export function Projects() {
+  return (
+    <Stack>
+      <Title order={6}>My Projects</Title>
+      <ProjectsOverview />
+    </Stack>
+  )
+}
--- src/components/SnowUI/lib/style/SnowUITheme.ts
+++ src/components/SnowUI/lib/style/SnowUITheme.ts
@@ -1,6 +1,6 @@
 'use client'
 
-import {createTheme} from '@mantine/core'
+import {createTheme, rem} from '@mantine/core'
 
 import {components} from './customizations'
 
@@ -8,4 +8,15 @@ export const SnowUITheme = createTheme({
   components,
   primaryColor: 'gray',
   primaryShade: 9,
+
+  headings: {
+    sizes: {
+      h1: {fontSize: rem(64)},
+      h2: {fontSize: rem(48)},
+      h3: {fontSize: rem(24)},
+      h4: {fontSize: rem(18)},
+      h5: {fontSize: rem(14)},
+      h6: {fontSize: rem(12)},
+    },
+  },
 })
