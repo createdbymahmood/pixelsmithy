66bde3051f6281b0514b098e746ea6fbcb8598cc
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-12 22:16:56 +0330

    feat: add projects page title

--- src/app/snow-ui/projects/page.tsx
+++ src/app/snow-ui/projects/page.tsx
@@ -1,3 +1,8 @@
 import {Projects} from '@/components/SnowUI'
+import {constructMetadata} from '@/utils/constructMetadata'
+
+export const metadata = constructMetadata({
+  title: 'Projects',
+})
 
 export default Projects
