bdf447d933f0a9f171eb6716f0ab4beef0c64494
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-14 13:30:58 +0330

    feat: create ProjectTargets component

--- src/components/SnowUI/components/project/ProjectDetails/ProjectDetails.tsx
+++ src/components/SnowUI/components/project/ProjectDetails/ProjectDetails.tsx
@@ -1,12 +1,14 @@
 'use client'
 
+import {get} from 'lodash-es'
 import type {Params} from 'next/dist/shared/lib/router/utils/route-matcher'
 import {useParams} from 'next/navigation'
-import React from 'react'
+import React, {Fragment} from 'react'
 import type {ValuesType} from 'utility-types'
 
 import type {projectDetailsSections} from '@/components/SnowUI'
 import {ProjectOverview} from '@/components/SnowUI'
+import {ProjectTargets} from '@/components/SnowUI/components/project/ProjectTargets/ProjectTargets'
 
 interface QueryParams extends Params {
   section: ValuesType<typeof projectDetailsSections>
@@ -14,14 +16,19 @@ interface QueryParams extends Params {
 
 export function ProjectDetails() {
   const params = useParams<QueryParams>()
-  const element = (() => {
-    switch (params.section) {
-      case 'overview':
-        return <ProjectOverview />
+  const elementsMap: Record<
+    ValuesType<typeof projectDetailsSections>,
+    JSX.Element
+  > = {
+    activity: <Fragment />,
+    budget: <Fragment />,
+    files: <Fragment />,
+    settings: <Fragment />,
+    overview: <ProjectOverview />,
+    targets: <ProjectTargets />,
+    users: <Fragment />,
+  }
 
-      default:
-        break
-    }
-  })()
+  const element = get(elementsMap, params.section, null)
   return element
 }
