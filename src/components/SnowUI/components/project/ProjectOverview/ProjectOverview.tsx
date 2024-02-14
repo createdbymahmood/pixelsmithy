5fca5b8ee21d1cf0d3fe34a8eed5dd7c42feac58
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-14 10:47:05 +0330

    feat: add ProjectOverviewFilesList

--- src/components/SnowUI/components/project/ProjectOverview/ProjectOverview.tsx
+++ src/components/SnowUI/components/project/ProjectOverview/ProjectOverview.tsx
@@ -1,11 +1,15 @@
 import {SimpleGrid} from '@mantine/core'
 
-import {ProjectOverviewTimeline} from '@/components/SnowUI/components/project/ProjectOverview/components'
+import {
+  ProjectOverviewFilesList,
+  ProjectOverviewTimeline,
+} from '@/components/SnowUI/components/project/ProjectOverview/components'
 
 export function ProjectOverview() {
   return (
-    <SimpleGrid cols={{xs: 1, sm: 2, md: 2}}>
+    <SimpleGrid cols={{md: 1, lg: 2}}>
       <ProjectOverviewTimeline />
+      <ProjectOverviewFilesList />
     </SimpleGrid>
   )
 }
