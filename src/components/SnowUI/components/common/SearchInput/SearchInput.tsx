96e3758f554b1c4a29853fd54d8dae1c55a30a7f
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-14 23:33:15 +0330

    feat: add ProjectUsers

---
+++ src/components/SnowUI/components/common/SearchInput/SearchInput.tsx
@@ -0,0 +1,26 @@
+import type {InputProps} from '@mantine/core'
+import {Input} from '@mantine/core'
+import {MagnifyingGlass} from '@phosphor-icons/react'
+import React from 'react'
+
+import {inter} from '@/components/SnowUI/layout/SnowUIProviders'
+
+import styles from './SearchInput.module.scss'
+
+interface SearchInputProps extends InputProps {}
+
+export function SearchInput(props: SearchInputProps) {
+  return (
+    <Input
+      className={inter.className}
+      classNames={{
+        input: styles.searchInput,
+        wrapper: styles.searchInputWrapper,
+      }}
+      leftSection={<MagnifyingGlass />}
+      placeholder='Search'
+      size='xs'
+      {...props}
+    />
+  )
+}
---
+++ src/components/SnowUI/hooks/useTableState.ts
@@ -0,0 +1,25 @@
+import {useSelections} from 'ahooks'
+import type {ChangeEvent} from 'react'
+
+export function useTableState<T>(items: T[], defaultSelected?: T[]) {
+  const selections = useSelections<T>(items, defaultSelected)
+
+  const onAllSelectionsChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.currentTarget.checked) {
+      selections.selectAll()
+    } else {
+      selections.unSelectAll()
+    }
+  }
+
+  const onItemSelectionChange =
+    (value: T) => (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.currentTarget.checked) {
+        selections.select(value)
+      } else {
+        selections.unSelect(value)
+      }
+    }
+
+  return {onAllSelectionsChange, onItemSelectionChange, selections}
+}
