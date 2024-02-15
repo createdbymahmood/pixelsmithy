218c642a64beade6ae6eaa6689b316254a80ab42
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-16 00:11:56 +0330

    feat: add ProfileDetails components

--- src/app/snow-ui/account/overview/page.tsx
+++ src/app/snow-ui/account/overview/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import {AccountOverview} from '@/components/SnowUI'
+import {constructMetadata} from '@/utils/constructMetadata'
+
+export const metadata = constructMetadata({
+  title: 'Account overview',
+})
+
 export default function Overview() {
-  return <div>Overview</div>
+  return <AccountOverview />
 }
---
+++ src/components/SnowUI/components/account/AccountOverview/components/ProfileDetails/ProfileDetails.tsx
@@ -0,0 +1,91 @@
+import {
+  Alert,
+  Badge,
+  Card,
+  Group,
+  Stack,
+  Table,
+  Text,
+  Title,
+} from '@mantine/core'
+import {Info} from '@phosphor-icons/react/dist/ssr'
+
+import styles from './ProfileDetails.module.scss'
+
+function PaymentMethodAlert() {
+  return (
+    <Alert
+      bg='indigo.0'
+      classNames={{
+        icon: styles.alertIcon,
+        root: styles.alertRoot,
+        message: styles.alertMessage,
+        title: styles.alertTitle,
+        body: styles.alertBody,
+      }}
+      icon={<Info size={20} />}
+      title='We need your attention!'
+    >
+      Your payment was declined. To start using tools, please Add Payment
+      Method.
+    </Alert>
+  )
+}
+
+function Header() {
+  return (
+    <Group gap='xs'>
+      <Title order={5}>Profile Details</Title>
+      <Text c='dimmed' className={styles.edit} size='sm'>
+        Edit Profile
+      </Text>
+    </Group>
+  )
+}
+
+function Details() {
+  const elements = [
+    {key: 'Company', value: 'Snow UI'},
+    {key: 'Contact phone', value: '+852 19850622', isVerified: true},
+    {key: 'Company Site', value: 'snowui.byewind.com'},
+    {key: 'Country', value: 'United States'},
+    {key: 'Communication', value: 'Email, Phone'},
+    {key: 'Allow Changes', value: 'Yes'},
+  ]
+
+  const content = elements.map((element) => {
+    return (
+      <Table.Tr key={element.key}>
+        <Table.Td>
+          <Text c='dimmed' size='xs'>
+            {element.key}
+          </Text>
+        </Table.Td>
+
+        <Table.Td>
+          <Group gap='xs'>
+            <Text size='xs'>{element.value}</Text>
+            {element.isVerified ? (
+              <Badge className={styles.verifiedBadge} size='sm'>
+                Verified
+              </Badge>
+            ) : null}
+          </Group>
+        </Table.Td>
+      </Table.Tr>
+    )
+  })
+  return <Table horizontalSpacing={0}>{content}</Table>
+}
+
+export function ProfileDetails() {
+  return (
+    <Card>
+      <Stack>
+        <Header />
+        <Details />
+        <PaymentMethodAlert />
+      </Stack>
+    </Card>
+  )
+}
