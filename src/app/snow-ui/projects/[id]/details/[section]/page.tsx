0ea0c4ffe941654732178b9e59f932b533fc7d6b
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-15 12:56:34 +0330

    refactor: a better page title handling

--- src/app/snow-ui/projects/[id]/details/[section]/page.tsx
+++ src/app/snow-ui/projects/[id]/details/[section]/page.tsx
@@ -1,9 +1,7 @@
-import {get} from 'lodash-es'
+import {startCase} from 'lodash-es'
 import type {Metadata} from 'next'
-import type {ValuesType} from 'utility-types'
 
 import {sleep} from '@/app/snow-ui/projects/page'
-import type {projectDetailsSections} from '@/components/SnowUI'
 import {ProjectDetails as ProjectDetailsImpl} from '@/components/SnowUI/components/project/ProjectDetails/ProjectDetails'
 import {defaultLoadingScreenDelayMS} from '@/constants'
 import {constructMetadata} from '@/utils/constructMetadata'
@@ -12,24 +10,13 @@ interface Props {
   params: {section: string}
 }
 
+const generateSectionTitle = (section: string) =>
+  `Project ${startCase(section)}`
+
 export function generateMetadata({params}: Props): Metadata {
-  const section = params.section
-
-  const metadataMap: Record<
-    ValuesType<typeof projectDetailsSections>,
-    Metadata
-  > = {
-    activity: constructMetadata({title: 'Project Activity'}),
-    budget: constructMetadata({title: 'Project Budget'}),
-    files: constructMetadata({title: 'Project Files'}),
-    overview: constructMetadata({title: 'Project Overview'}),
-    settings: constructMetadata({title: 'Project Settings'}),
-    targets: constructMetadata({title: 'Project Targets'}),
-    users: constructMetadata({title: 'Project Users'}),
-  }
-
-  const metadata = get(metadataMap, section)
-  return metadata
+  return constructMetadata({
+    title: generateSectionTitle(params.section),
+  })
 }
 
 export default async function ProjectDetails() {
