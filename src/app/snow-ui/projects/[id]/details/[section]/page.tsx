11a5d4e0cc5667ba9c27725e57b3e10e76f36657
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-15 12:04:47 +0330

    feat: add project sections page titles

--- src/app/snow-ui/projects/[id]/details/[section]/page.tsx
+++ src/app/snow-ui/projects/[id]/details/[section]/page.tsx
@@ -1,8 +1,33 @@
+import {get} from 'lodash-es'
+import type {Metadata} from 'next'
+import type {ValuesType} from 'utility-types'
+
+import type {projectDetailsSections} from '@/components/SnowUI'
 import {ProjectDetails} from '@/components/SnowUI/components/project/ProjectDetails/ProjectDetails'
 import {constructMetadata} from '@/utils/constructMetadata'
 
-export const metadata = constructMetadata({
-  title: 'Project Overview',
-})
+interface Props {
+  params: {section: string}
+}
+
+export function generateMetadata({params}: Props): Metadata {
+  const section = params.section
+
+  const metadataMap: Record<
+    ValuesType<typeof projectDetailsSections>,
+    Metadata
+  > = {
+    activity: constructMetadata({title: 'Project Activity'}),
+    budget: constructMetadata({title: 'Project Budget'}),
+    files: constructMetadata({title: 'Project Files'}),
+    overview: constructMetadata({title: 'Project Overview'}),
+    settings: constructMetadata({title: 'Project Settings'}),
+    targets: constructMetadata({title: 'Project Targets'}),
+    users: constructMetadata({title: 'Project Users'}),
+  }
+
+  const metadata = get(metadataMap, section)
+  return metadata
+}
 
 export default ProjectDetails
