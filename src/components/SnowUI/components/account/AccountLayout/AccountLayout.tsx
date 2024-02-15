f2b6d8ffcf7e9028b6717bf86a8726792e463ee8
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-15 14:26:23 +0330

    feat: add separate pages for each section of account

--- src/components/SnowUI/components/account/AccountLayout/AccountLayout.tsx
+++ src/components/SnowUI/components/account/AccountLayout/AccountLayout.tsx
@@ -7,7 +7,11 @@ import {Box, Tabs} from '@mantine/core'
 import {startCase} from 'lodash-es'
 import type {Params} from 'next/dist/shared/lib/router/utils/route-matcher'
 import Link from 'next/link'
-import {useParams} from 'next/navigation'
+import {
+  useParams,
+  useSelectedLayoutSegment,
+  useSelectedLayoutSegments,
+} from 'next/navigation'
 import type {ReactNode} from 'react'
 
 import {urls} from '@/constants'
@@ -29,10 +33,6 @@ const accountSections = [
   'hire-me',
 ] as const
 
-interface QueryParams extends Params {
-  section: string
-}
-
 const Tab = Tabs.Tab as MantineComponent<{
   props: TabsTabProps & {href: string}
   ref: HTMLButtonElement
@@ -41,7 +41,7 @@ const Tab = Tabs.Tab as MantineComponent<{
 }>
 
 export function AccountLayout({children}: ProjectOverviewProps) {
-  const params = useParams<QueryParams>()
+  const segment = useSelectedLayoutSegment()
 
   const content = accountSections.map((tab) => {
     return (
@@ -57,7 +57,7 @@ export function AccountLayout({children}: ProjectOverviewProps) {
   })
 
   return (
-    <Tabs defaultValue={params.section}>
+    <Tabs defaultValue={segment}>
       <Tabs.List>{content}</Tabs.List>
       <Box mt='lg'>{children}</Box>
     </Tabs>
