645331ff4233562335dd3bc257003e96eefe4a3e
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-16 16:52:10 +0330

    refactor: cleanup previous CSR based content of account

--- src/app/snow-ui/account/x/layout.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-
-import {AccountLayout} from '@/components/SnowUI/components/account'
-
-export default AccountLayout
--- src/app/snow-ui/account/x/loading.tsx
+++
@@ -1,5 +0,0 @@
-import {Loader} from '@mantine/core'
-
-export default function Loading() {
-  return <Loader size='xs' />
-}
