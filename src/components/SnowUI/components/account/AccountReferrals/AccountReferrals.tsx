2ad3e0a053468ddc7d85e131e55fa670a990d1a5
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-18 18:59:17 +0330

    feat: add ReferralProgram

---
+++ src/components/SnowUI/components/account/AccountReferrals/AccountReferrals.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import {Stack} from '@mantine/core'
+import React from 'react'
+
+import {ReferralProgram} from './components'
+
+export function AccountReferrals() {
+  return (
+    <Stack>
+      <ReferralProgram />
+    </Stack>
+  )
+}
