44881b3d3ac59643c5f275102c8b11d5bb5190b2
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-18 18:59:17 +0330

    feat: add ReferralProgram

--- src/app/snow-ui/account/referrals/page.tsx
+++ src/app/snow-ui/account/referrals/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import {AccountReferrals} from '@/components/SnowUI'
+import {constructMetadata} from '@/utils/constructMetadata'
+
+export const metadata = constructMetadata({
+  title: 'Account Referrals',
+})
+
 export default function Referrals() {
-  return <div>Referrals</div>
+  return <AccountReferrals />
 }
--- src/components/SnowUI/components/account/index.ts
+++ src/components/SnowUI/components/account/index.ts
@@ -1,6 +1,7 @@
 export * from './AccountBilling'
 export * from './AccountLayout'
 export * from './AccountOverview'
+export * from './AccountReferrals'
 export * from './AccountSecurity'
 export * from './AccountSettings'
 export * from './AccountStatements'
