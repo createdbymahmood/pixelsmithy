091ed00ef8d04dafc16698104abc5d1c6d41b05c
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-18 18:00:03 +0330

    feat: add Earnings

--- src/app/snow-ui/account/statements/page.tsx
+++ src/app/snow-ui/account/statements/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import {AccountStatements} from '@/components/SnowUI'
+import {constructMetadata} from '@/utils/constructMetadata'
+
+export const metadata = constructMetadata({
+  title: 'Account Statements',
+})
+
 export default function Statements() {
-  return <div>Statements</div>
+  return <AccountStatements />
 }
