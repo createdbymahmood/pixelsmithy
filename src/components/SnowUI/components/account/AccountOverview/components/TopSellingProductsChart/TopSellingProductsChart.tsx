059f8d60f1876fd60b52a8a1e13dae30896fb94c
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-19 23:57:42 +0330

    feat: add skeleton instead of gray BG placeholder

--- src/components/SnowUI/components/account/AccountOverview/components/TopSellingProductsChart/TopSellingProductsChart.tsx
+++ src/components/SnowUI/components/account/AccountOverview/components/TopSellingProductsChart/TopSellingProductsChart.tsx
@@ -1,10 +1,13 @@
-import {Card, rem, Title} from '@mantine/core'
+import {Card, rem, Skeleton, Stack, Title} from '@mantine/core'
 import React from 'react'
 
 export function TopSellingProductsChart() {
   return (
-    <Card h={rem(312)}>
-      <Title order={5}>Top Selling Products</Title>
+    <Card display='flex' h={rem(312)}>
+      <Stack h='100%'>
+        <Title order={5}>Top Selling Products</Title>
+        <Skeleton h='100%' radius='lg' />
+      </Stack>
     </Card>
   )
 }
