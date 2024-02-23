98cbbce0c8ebe08e721b104f1347908f2a8fda26
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-23 12:54:38 +0330

    refactor: cleanup layouts

--- src/app/snow-ui/dashboard/email/layout.tsx
+++ src/app/snow-ui/dashboard/email/layout.tsx
@@ -1,18 +1,5 @@
 'use client'
 
-import type {ReactNode} from 'react'
-
 import {EmailLayout} from '@/components/SnowUI'
-import {SnowUIDashboardLayout} from '@/components/SnowUI/layout/SnowUIDashboardLayout'
 
-interface LayoutProps {
-  children: ReactNode
-}
-
-export default function Layout({children}: LayoutProps) {
-  return (
-    <SnowUIDashboardLayout withNotificationsSidebar={false}>
-      <EmailLayout>{children}</EmailLayout>
-    </SnowUIDashboardLayout>
-  )
-}
+export default EmailLayout
--- src/app/snow-ui/dashboard/user-profile/layout.tsx
+++ src/app/snow-ui/dashboard/user-profile/layout.tsx
@@ -1,17 +1,4 @@
 'use client'
-import type {ReactNode} from 'react'
-
 import {UserProfileLayout} from '@/components/SnowUI'
-import {SnowUIDashboardLayout} from '@/components/SnowUI/layout/SnowUIDashboardLayout'
 
-interface LayoutProps {
-  children: ReactNode
-}
-
-export default function Layout({children}: LayoutProps) {
-  return (
-    <SnowUIDashboardLayout withNotificationsSidebar={false}>
-      <UserProfileLayout>{children}</UserProfileLayout>
-    </SnowUIDashboardLayout>
-  )
-}
+export default UserProfileLayout
