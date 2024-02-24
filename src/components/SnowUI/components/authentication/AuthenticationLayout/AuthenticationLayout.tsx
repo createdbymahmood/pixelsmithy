d37e9e0d0f4dd21a5c04f8d8caee92c36ef1157a
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-24 10:44:49 +0330

    feat: add auth layout

---
+++ src/components/SnowUI/components/authentication/AuthenticationLayout/AuthenticationLayout.tsx
@@ -0,0 +1,74 @@
+import {
+  Button,
+  Card,
+  Group,
+  rem,
+  SimpleGrid,
+  Skeleton,
+  Stack,
+  Text,
+} from '@mantine/core'
+import type {ReactNode} from 'react'
+
+const navbar = ['Product', 'Solutions', 'Resources', 'Download', 'Pricing']
+
+function Navbar() {
+  const content = navbar.map((navbarItem) => {
+    return (
+      <Text key={navbarItem} size='sm'>
+        {navbarItem}
+      </Text>
+    )
+  })
+  return <Group gap='xl'>{content}</Group>
+}
+
+function SignInActions() {
+  return (
+    <Group justify='flex-end'>
+      <Button size='xs'>Sign in</Button>
+      <Button size='xs' variant='light'>
+        Sign up
+      </Button>
+    </Group>
+  )
+}
+
+function Logo() {
+  return <Skeleton h={rem(28)} w={rem(105)} />
+}
+
+function Header() {
+  return (
+    <SimpleGrid cols={{md: 3}} w='100%'>
+      <Logo />
+      <Navbar />
+      <SignInActions />
+    </SimpleGrid>
+  )
+}
+
+interface ContentProps {
+  children: ReactNode
+}
+
+function Content({children}: ContentProps) {
+  return (
+    <Card bg='white' h={658} miw={rem(680)} p={rem(100)}>
+      {children}
+    </Card>
+  )
+}
+
+interface AuthenticationLayoutProps {
+  children: ReactNode
+}
+
+export function AuthenticationLayout(props: AuthenticationLayoutProps) {
+  return (
+    <Stack align='center' bg='gray.1' mih='100vh' miw='100vw' p='xl'>
+      <Header />
+      <Content>{props.children}</Content>
+    </Stack>
+  )
+}
