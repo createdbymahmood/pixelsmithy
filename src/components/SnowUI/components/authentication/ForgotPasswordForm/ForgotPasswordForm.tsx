1c44b3f2e4da35e9dbb37fe05d9846a1583ceeb3
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-24 21:00:57 +0330

    feat: add forgot password page

---
+++ src/components/SnowUI/components/authentication/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -0,0 +1,61 @@
+import {Anchor, Button, Input, rem, Stack, Text, Title} from '@mantine/core'
+import Link from 'next/link'
+import React from 'react'
+
+import {urls} from '@/constants'
+
+import styles from './ForgotPasswordForm.module.scss'
+
+function Header() {
+  return (
+    <Stack align='center' gap={rem(5)}>
+      <Title order={3}>Forgot Password?</Title>
+      <Text c='dimmed' size='sm'>
+        Enter your email to reset your password.
+      </Text>
+    </Stack>
+  )
+}
+
+function FormView() {
+  return (
+    <Stack w={rem(360)}>
+      <Input
+        classNames={{input: styles.input}}
+        placeholder='Please enter your email address'
+        radius='md'
+        w='100%'
+      />
+
+      <Button mt='xs' radius='md' fullWidth>
+        Submit
+      </Button>
+    </Stack>
+  )
+}
+
+function Footer() {
+  return (
+    <Anchor
+      c='indigo.4'
+      className={styles.forgotPassword}
+      component={Link}
+      href={urls.SnowUI.authentication.signIn}
+      mt='sm'
+      size='sm'
+      underline='never'
+    >
+      Back
+    </Anchor>
+  )
+}
+
+export function ForgotPasswordForm() {
+  return (
+    <Stack align='center'>
+      <Header />
+      <FormView />
+      <Footer />
+    </Stack>
+  )
+}
