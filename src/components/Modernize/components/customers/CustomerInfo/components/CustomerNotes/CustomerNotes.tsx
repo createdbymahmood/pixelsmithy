930a93fdc97cc87eb787cf51f87388fde5b0cbb9
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-27 19:40:04 +0330

    feat: add more on customer notes and customer overview

--- src/components/Modernize/components/customers/CustomerInfo/components/CustomerNotes/CustomerNotes.tsx
+++ src/components/Modernize/components/customers/CustomerInfo/components/CustomerNotes/CustomerNotes.tsx
@@ -1,6 +1,54 @@
-import {Card} from '@mantine/core'
+'use client'
+
+import {
+  Avatar,
+  Card,
+  Divider,
+  Group,
+  Rating,
+  rem,
+  Stack,
+  Text,
+  Textarea,
+} from '@mantine/core'
 import React from 'react'
 
+function Header() {
+  return (
+    <Group align='flex-start' gap={rem(18)}>
+      <Avatar size={rem(56)} variant='light'>
+        L
+      </Avatar>
+      <Stack gap={rem(8)}>
+        <Text fw='bold'>Lenora Robinson</Text>
+        <Text
+          c='general.5'
+          className='white-space-pre-line'
+          size='sm'
+        >{`Ireland\n5 Orders\nCustomer for 2 years`}</Text>
+      </Stack>
+      <Rating defaultValue={2} ml='auto' size='md' />
+    </Group>
+  )
+}
+
+function Notes() {
+  return (
+    <Stack gap={rem(24)}>
+      <Text fw='bold'>Customer Notes</Text>
+      <Textarea label='Notes' placeholder='Add notes about customer' />
+    </Stack>
+  )
+}
+
 export function CustomerNotes() {
-  return <Card>CustomerNotes</Card>
+  return (
+    <Card>
+      <Stack gap={rem(28)}>
+        <Header />
+        <Divider />
+        <Notes />
+      </Stack>
+    </Card>
+  )
 }
--- src/components/Modernize/components/customers/CustomerInfo/components/CustomerOverview/CustomerOverview.tsx
+++ src/components/Modernize/components/customers/CustomerInfo/components/CustomerOverview/CustomerOverview.tsx
@@ -1,6 +1,80 @@
-import {Card} from '@mantine/core'
+import {
+  Anchor,
+  Button,
+  Card,
+  Divider,
+  Group,
+  rem,
+  Stack,
+  Text,
+} from '@mantine/core'
 import React from 'react'
 
+function Header() {
+  return (
+    <Group justify='space-between'>
+      <Text fw='bold'>Overview</Text>
+
+      <Anchor c='primary' underline='never'>
+        Edit
+      </Anchor>
+    </Group>
+  )
+}
+
+function Body() {
+  return (
+    <Stack>
+      <Stack gap={rem(7)}>
+        <Text c='general.3' size='sm'>
+          Address
+        </Text>
+
+        <Text c='general.5' className='pre-line' size='sm'>
+          {`831 Wilhelmine Glen\n40583-2293\nSouth Lelastad Ireland`}
+        </Text>
+      </Stack>
+
+      <Stack gap={rem(7)}>
+        <Text c='general.3' size='sm'>
+          Email Address
+        </Text>
+
+        <Text c='general.5' size='sm'>
+          lenora_rob@yahoo.com
+        </Text>
+      </Stack>
+
+      <Stack gap={rem(7)}>
+        <Text c='general.3' size='sm'>
+          Phone
+        </Text>
+
+        <Text c='general.5' size='sm'>
+          636-458-4820
+        </Text>
+      </Stack>
+    </Stack>
+  )
+}
+
+function Footer() {
+  return (
+    <Anchor c='red' size='sm' underline='never'>
+      Delete Customer
+    </Anchor>
+  )
+}
+
 export function CustomerOverview() {
-  return <Card>CustomerOverview</Card>
+  return (
+    <Card>
+      <Stack gap={rem(24)}>
+        <Header />
+        <Body />
+        <Divider />
+        <Footer />
+      </Stack>
+    </Card>
+  )
 }
