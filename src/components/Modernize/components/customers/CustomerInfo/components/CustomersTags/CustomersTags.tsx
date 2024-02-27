ed02a80cae4af986e7a30cf7e9aa4c7ff84767a2
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-02-27 19:46:11 +0330

    feat: add customer tags

--- src/components/Modernize/components/customers/CustomerInfo/components/CustomersTags/CustomersTags.tsx
+++ src/components/Modernize/components/customers/CustomerInfo/components/CustomersTags/CustomersTags.tsx
@@ -1,6 +1,33 @@
-import {Card} from '@mantine/core'
+import {Card, Chip, Group, Input, rem, Stack, Text} from '@mantine/core'
+import {X} from '@phosphor-icons/react/dist/ssr'
 import React from 'react'
 
 export function CustomersTags() {
-  return <Card>CustomersTags</Card>
+  const tags = ['Vip Customer', 'Europe']
+  const content = tags.map((tag) => {
+    return (
+      <Chip
+        key={tag}
+        color='general.5'
+        icon={<X weight='bold' />}
+        radius='sm'
+        variant='light'
+        defaultChecked
+      >
+        T-{tag}
+      </Chip>
+    )
+  })
+
+  return (
+    <Card>
+      <Stack gap={rem(24)}>
+        <Text fw='bold'>Tags</Text>
+        <Input.Wrapper label='Add Tags' w='100%'>
+          <Input placeholder='Enter tag name' type='text' />
+        </Input.Wrapper>
+        <Group gap={rem(8)}>{content}</Group>
+      </Stack>
+    </Card>
+  )
 }
