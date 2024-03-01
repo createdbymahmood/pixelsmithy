98c70ec28be2cec86e5e07d0383274b2ac745a06
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-01 10:08:01 +0330

    feat: add bare minimum of dashboard states

--- src/app/modernize/dashboard/page.tsx
+++ src/app/modernize/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import {Dashboard as DashboardView} from '@modernize/components'
 import React from 'react'
 
+import {constructMetadata} from '@/utils/constructMetadata'
+
+export const metadata = constructMetadata({
+  title: 'Dashboard',
+})
+
 export default function Dashboard() {
-  return <div style={{height: '200vh'}} />
+  return <DashboardView />
 }
