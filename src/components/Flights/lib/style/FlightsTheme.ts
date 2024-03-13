e45ff37164ff6161fb175a87482f401e5a0c10ac
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-14 00:39:48 +0330

    feat: add flights theme colors

--- src/components/Flights/lib/style/FlightsTheme.ts
+++ src/components/Flights/lib/style/FlightsTheme.ts
@@ -2,9 +2,12 @@ import {createTheme} from '@mantine/core'
 
 import {inter} from '@/lib/styles/font/inter'
 
+import {colors} from './colors'
+
 const fontFamily = inter.style.fontFamily
 
 export const FlightsTheme = createTheme({
+  colors,
   fontFamily,
   headings: {
     fontFamily,
