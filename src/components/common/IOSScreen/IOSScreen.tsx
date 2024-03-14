aa0f21c0fa58c4b94a7e6fcbff1f25f58f4035ed
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-14 04:51:17 +0330

    feat: add IOSScreen component

---
+++ src/components/common/IOSScreen/IOSScreen.tsx
@@ -0,0 +1,87 @@
+import Battery from '@flights/assets/icons/battery.svg'
+import type {MantineColor, StackProps} from '@mantine/core'
+import {Box, Group, rem, Stack, Text} from '@mantine/core'
+import {omit} from 'lodash-es'
+import type {ReactNode} from 'react'
+import React from 'react'
+
+import styles from './IOSScreen.module.scss'
+
+interface IOSScreenProps extends StackProps {
+  children: ReactNode
+  variant?: 'dark' | 'light'
+}
+
+interface Props {
+  color: MantineColor
+}
+
+function StatusBar({color}: Props) {
+  return (
+    <Group
+      c={color}
+      className='shrink-0'
+      justify='space-between'
+      left={0}
+      pos='absolute'
+      px={rem(27)}
+      py='xs'
+      right={0}
+      top={0}
+    >
+      <Text c='currentColor' fw='600'>
+        9:41
+      </Text>
+
+      <Group>
+        <Box c={color} component={Battery} />
+      </Group>
+    </Group>
+  )
+}
+
+function HomeIndicator({color}: Props) {
+  return (
+    <Group
+      align='flex-end'
+      bottom={0}
+      c={color}
+      h={rem(34)}
+      justify='center'
+      left={0}
+      mb={rem(8)}
+      pos='absolute'
+      right={0}
+    >
+      <Box
+        bg='currentColor'
+        className={styles.homeIndicator}
+        h={rem(5)}
+        w={rem(134)}
+      />
+    </Group>
+  )
+}
+
+export function IOSScreen({
+  children,
+  variant = 'light',
+  ...props
+}: IOSScreenProps) {
+  const color: MantineColor = variant === 'dark' ? 'black' : 'white'
+
+  return (
+    <Stack
+      color={color}
+      h={rem(844)}
+      justify='center'
+      pos='relative'
+      w={rem(390)}
+      {...omit(props, ['h', 'w'])}
+    >
+      <StatusBar color={color} />
+      <Box h={`calc(100% - ${rem(47 + 34)})`}>{children}</Box>
+      <HomeIndicator color={color} />
+    </Stack>
+  )
+}
