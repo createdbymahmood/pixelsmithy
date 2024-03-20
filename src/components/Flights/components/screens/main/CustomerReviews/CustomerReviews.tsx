c6a2bb1cf2576acfcfc867e768f889ab257dd427
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-20 20:45:04 +0330

    refactor: some cleanups and abstractions on CustomerRerview Page

--- src/components/Flights/components/screens/main/CustomerReviews/CustomerReviews.tsx
+++ src/components/Flights/components/screens/main/CustomerReviews/CustomerReviews.tsx
@@ -7,12 +7,18 @@ import {ScreenHeader} from '@/components/Flights/components/common'
 import {CustomerReview} from '@/components/Flights/components/screens'
 
 export function CustomerReviews() {
+  const homeIndicatorSize = 34
+  const screenHeaderSize = 24
+  const contentOffset = 40
+  /* prettier-ignore */
+  const contentHeight = `calc(100% - (${rem(homeIndicatorSize + screenHeaderSize + contentOffset)} + var(--mantine-spacing-xxxl)))`
+
   return (
     <IOSScreen variant='dark'>
       <Stack gap='md' h='100%' mt={rem(63)} pb={0} px='md'>
         <ScreenHeader title='Customer reviews' />
         <ScrollableContent
-          h='100%'
+          h={contentHeight}
           mx='calc(var(--mantine-spacing-md) * -1)'
           px='md'
         >
