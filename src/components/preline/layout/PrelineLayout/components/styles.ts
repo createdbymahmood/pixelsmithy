6bded67d88d00fb2390eebaf867a54de62bdab04
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-30 19:35:24 +0330

    refactor: separate style to another file

---
+++ src/components/preline/layout/PrelineLayout/components/styles.ts
@@ -0,0 +1,34 @@
+import {
+  AccordionPanel as OriginalAccordionPanel,
+  chakra,
+  Text,
+} from '@chakra-ui/react'
+
+export const AccordionPanel = chakra(OriginalAccordionPanel, {
+  baseStyle: {
+    position: 'relative',
+    _before: {
+      content: "''",
+      display: 'block',
+      width: 0.5,
+      borderRadius: 'xl',
+      bg: 'gray.200',
+      position: 'absolute',
+      left: -3,
+      top: 1,
+      bottom: 1,
+    },
+  },
+})
+
+export const SectionTitle = chakra(Text, {
+  baseStyle: {
+    color: 'gray.500',
+    fontSize: 'xs',
+    fontWeight: '600',
+    mb: 2,
+    ml: 4,
+    mt: 4,
+    textTransform: 'uppercase',
+  },
+})
