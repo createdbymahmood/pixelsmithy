20ca3497e9a741007c544e9333c750da5bd02403
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-31 20:15:04 +0330

    feat: add bare minimum of themification for job huntly

--- src/components/job-huntly/lib/style/JobHuntlyTheme.ts
+++ src/components/job-huntly/lib/style/JobHuntlyTheme.ts
@@ -1,9 +1,22 @@
-import {createTheme} from '@mantine/core'
+import {createTheme, rem} from '@mantine/core'
 
 import {inter} from '@/lib/styles/font/inter'
+
+import {colors} from './colors'
 
 const fontFamily = inter.style.fontFamily
 
 export const JobHuntlyTheme = createTheme({
   fontFamily,
+  colors,
+  headings: {
+    fontFamily,
+    sizes: {
+      h1: {fontSize: rem(72), lineHeight: rem(79.2), fontWeight: '700'},
+      h2: {fontSize: rem(48), lineHeight: rem(52.8), fontWeight: '700'},
+      h3: {fontSize: rem(32), lineHeight: rem(38.4), fontWeight: '700'},
+      h4: {fontSize: rem(24), lineHeight: rem(28.8), fontWeight: '700'},
+      h5: {fontSize: rem(20), lineHeight: rem(24), fontWeight: '700'},
+    },
+  },
 })
