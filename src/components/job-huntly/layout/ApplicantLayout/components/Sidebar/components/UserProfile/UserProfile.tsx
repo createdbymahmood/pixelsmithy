a15978e639544de380001352bf1088766b3d89b0
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-03-31 22:23:16 +0330

    feat: update user profile in sidebar

--- src/components/job-huntly/layout/ApplicantLayout/components/Sidebar/components/UserProfile/UserProfile.tsx
+++ src/components/job-huntly/layout/ApplicantLayout/components/Sidebar/components/UserProfile/UserProfile.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 export function UserProfile() {
   return (
     <Group>
-      <Avatar size={rem(48)} />
+      <Avatar size={rem(48)}>JG</Avatar>
       <Stack gap={0}>
         <Text fw='600' size='lg'>
           Jake Gyll
         </Text>
-        <Text size='sm'>jakegyll@email.com</Text>
+        <Text c='neutrals.4' size='sm'>
+          jakegyll@email.com
+        </Text>
       </Stack>
     </Group>
   )
--- src/components/job-huntly/layout/ApplicantLayout/components/Sidebar/components/UserProfile/index.ts
+++ src/components/job-huntly/layout/ApplicantLayout/components/Sidebar/components/UserProfile/index.ts
@@ -1,1 +1,1 @@
-export * from './SidebarUserProfile'
+export * from './UserProfile'
