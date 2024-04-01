bfe45b9afcad9e4cd97c69dec191d37aecffd6ba
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-01 22:23:00 +0330

    feat: add more realistic mocks to applications list

--- src/components/job-huntly/mock/applications/applications.ts
+++ src/components/job-huntly/mock/applications/applications.ts
@@ -1,3 +1,8 @@
+import type {FC, SVGProps} from 'react'
+
+import Nomad from './assets/logo/nomad.svg'
+import Packer from './assets/logo/packer.svg'
+
 export type ApplicationStatus = 'declined' | 'review' | 'shortlisted'
 export interface Application {
   id: string
@@ -5,7 +10,9 @@ export interface Application {
   info: string
   date: string
   status: ApplicationStatus
+  logo: FC<SVGProps<SVGElement>>
 }
+
 export const applications: Application[] = [
   {
     id: '1',
@@ -13,6 +20,7 @@ export const applications: Application[] = [
     info: 'Nomad • Paris, France • Full-Time',
     position: 'Social Media Assistant',
     status: 'review',
+    logo: Nomad,
   },
   {
     id: '2',
@@ -20,6 +28,7 @@ export const applications: Application[] = [
     info: 'Udacity • New York, USA • Full-Time',
     position: 'Social Media Assistant',
     status: 'shortlisted',
+    logo: Nomad,
   },
   {
     id: '3',
@@ -27,5 +36,6 @@ export const applications: Application[] = [
     info: 'Packer • Madrid, Spain • Full-Time',
     position: 'Social Media Assistant',
     status: 'declined',
+    logo: Packer,
   },
 ]
