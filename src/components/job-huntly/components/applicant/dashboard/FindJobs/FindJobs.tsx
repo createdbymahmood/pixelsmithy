c0d0aad6362425c0eedb0633b3026c4ca6c5f83d
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-07 02:25:46 +0330

    feat: add job filters

--- src/components/job-huntly/components/applicant/dashboard/FindJobs/FindJobs.tsx
+++ src/components/job-huntly/components/applicant/dashboard/FindJobs/FindJobs.tsx
@@ -1,12 +1,19 @@
-import {Stack} from '@mantine/core'
+import {Group, rem, Stack} from '@mantine/core'
 import React from 'react'
 
-import {SearchBox} from './components'
+import {JobFilters, SearchBox} from './components'
+
+const SEARCH_BOX_HEIGHT = rem(141.59)
+const contentHeight = `calc(100% - (${SEARCH_BOX_HEIGHT} + var(--mantine-spacing-xxxl))`
 
 export function FindJobs() {
   return (
-    <Stack gap='xxxl'>
+    <Stack className='overflow-y-hidden' gap='xxxl' h='100%'>
       <SearchBox />
+
+      <Group gap='xxl' h={contentHeight} wrap='nowrap'>
+        <JobFilters />
+      </Group>
     </Stack>
   )
 }
