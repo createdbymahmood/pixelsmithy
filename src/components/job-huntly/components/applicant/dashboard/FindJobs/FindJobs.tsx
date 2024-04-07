f95ba73251ddbc87612ff7f90253d367d9b7dd06
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-07 14:33:12 +0330

    feat: add divider

--- src/components/job-huntly/components/applicant/dashboard/FindJobs/FindJobs.tsx
+++ src/components/job-huntly/components/applicant/dashboard/FindJobs/FindJobs.tsx
@@ -1,16 +1,16 @@
-import {Group, rem, Stack} from '@mantine/core'
+import {Divider, Group, rem, Stack} from '@mantine/core'
 import React from 'react'
 
 import {JobFilters, JobsList, SearchBox} from './components'
 
 const SEARCH_BOX_HEIGHT = rem(141.59)
-const contentHeight = `calc(100% - (${SEARCH_BOX_HEIGHT} + var(--mantine-spacing-xxxl))`
+const contentHeight = `calc(100% - (${SEARCH_BOX_HEIGHT} + var(--mantine-spacing-xxxl) * 2)`
 
 export function FindJobs() {
   return (
     <Stack className='overflow-y-hidden' gap='xxxl' h='100%'>
       <SearchBox />
-
+      <Divider />
       <Group align='flex-start' gap='xxl' h={contentHeight} wrap='nowrap'>
         <JobFilters />
         <JobsList />
