5bd3fe5a682ff33bacf3ed8398690a8b244746f9
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-07 13:23:22 +0330

    feat: add jobs list Stacked card

--- src/components/job-huntly/components/applicant/dashboard/FindJobs/components/JobsList/JobsList.tsx
+++ src/components/job-huntly/components/applicant/dashboard/FindJobs/components/JobsList/JobsList.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import {Stack} from '@mantine/core'
 import React from 'react'
 
-import {Header} from './components'
+import {jobs} from '@/components/job-huntly/mock/jobs'
+
+import {Header, StackJobCard} from './components'
 
 export function JobsList() {
+  const list = jobs.map((job) => {
+    return <StackJobCard {...job} key={job.id} />
+  })
+
   return (
     <Stack gap='xxxl' w='100%'>
       <Header />
+
+      {list}
     </Stack>
   )
 }
