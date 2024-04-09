ed3ca8067d0eb3b5b8fe333a34845e07b0ed0025
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-09 20:33:47 +0330

    feat: add user profile About

---
+++ src/components/job-huntly/components/applicant/dashboard/Profile/components/About/About.tsx
@@ -0,0 +1,25 @@
+import {Stack, Text} from '@mantine/core'
+import React from 'react'
+
+import {EditButton, ProfileSection} from '../common'
+
+export function About() {
+  return (
+    <ProfileSection action={<EditButton />} title='About Me'>
+      <Stack>
+        <Text c='neutrals.5'>
+          I’m a product designer + filmmaker currently working remotely at
+          Twitter from beautiful Manchester, United Kingdom. I’m passionate
+          about designing digital products that have a positive impact on the
+          world.
+        </Text>
+
+        <Text c='neutrals.5'>
+          For 10 years, I’ve specialised in interface, experience & interaction
+          design as well as working in user research and product strategy for
+          product agencies, big tech companies & start-ups.
+        </Text>
+      </Stack>
+    </ProfileSection>
+  )
+}
