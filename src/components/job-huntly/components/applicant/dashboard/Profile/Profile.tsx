697825466aaa29a73cc8a93101217f7e73984bda
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-09 21:02:45 +0330

    feat: add experiences card

--- src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
+++ src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import {Grid} from '@mantine/core'
+import {Grid, Stack} from '@mantine/core'
 import React from 'react'
 
-import {About, Header} from './components'
+import {About, Experiences, Header} from './components'
 
 export function Profile() {
   return (
     <Grid>
       <Grid.Col span={{lg: 9, md: 12, base: 12}}>
-        <Header />
-        <About />
+        <Stack gap='xl'>
+          <Header />
+          <About />
+          <Experiences />
+        </Stack>
       </Grid.Col>
     </Grid>
   )
