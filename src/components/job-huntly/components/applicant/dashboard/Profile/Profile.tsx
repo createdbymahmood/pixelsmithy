f1d916b605f51f6b01e39a676304d341a516335f
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-09 20:33:47 +0330

    feat: add user profile About

--- src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
+++ src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
@@ -3,13 +3,14 @@
 import {Grid} from '@mantine/core'
 import React from 'react'
 
-import {Header} from './components'
+import {About, Header} from './components'
 
 export function Profile() {
   return (
     <Grid>
       <Grid.Col span={{lg: 9, md: 12, base: 12}}>
         <Header />
+        <About />
       </Grid.Col>
     </Grid>
   )
