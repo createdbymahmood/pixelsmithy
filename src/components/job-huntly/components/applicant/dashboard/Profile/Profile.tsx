34d50cde8a5f16241b2e5c72c9dbc58396b96a38
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-09 21:19:33 +0330

    feat: add user profile skills

--- src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
+++ src/components/job-huntly/components/applicant/dashboard/Profile/Profile.tsx
@@ -3,7 +3,7 @@
 import {Grid, Stack} from '@mantine/core'
 import React from 'react'
 
-import {About, Educations, Experiences, Header} from './components'
+import {About, Educations, Experiences, Header, Skills} from './components'
 
 export function Profile() {
   return (
@@ -14,6 +14,7 @@ export function Profile() {
           <About />
           <Experiences />
           <Educations />
+          <Skills />
         </Stack>
       </Grid.Col>
     </Grid>
