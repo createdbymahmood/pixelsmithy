76f94167f36b2d029540ac61dcb32db9028eb65d
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-11 12:13:26 +0330

    fix: remove help center items gap

--- src/components/job-huntly/components/applicant/dashboard/HelpCenter/HelpCenter.tsx
+++ src/components/job-huntly/components/applicant/dashboard/HelpCenter/HelpCenter.tsx
@@ -5,7 +5,7 @@ import {Sidebar} from './components'
 
 export function HelpCenter() {
   return (
-    <Group align='flex-start' h='100%' wrap='nowrap'>
+    <Group align='flex-start' gap={0} h='100%' wrap='nowrap'>
       <Sidebar />
       <Divider orientation='vertical' />
     </Group>
