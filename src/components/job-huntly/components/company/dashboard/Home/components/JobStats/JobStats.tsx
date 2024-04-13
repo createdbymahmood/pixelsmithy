d02b7236cca56ae417ff4dc1e918166a6bf193ac
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-13 11:55:39 +0330

    fix: polish job stats card header styles

--- src/components/job-huntly/components/company/dashboard/Home/components/JobStats/JobStats.tsx
+++ src/components/job-huntly/components/company/dashboard/Home/components/JobStats/JobStats.tsx
@@ -4,12 +4,12 @@ import React from 'react'
 function Header() {
   return (
     <Group justify='space-between'>
-      <Stack>
+      <Stack gap='xxs'>
         <Title order={5}>Job statistics</Title>
-        <Text>Showing Jobstatistic Jul 19-25</Text>
+        <Text c='neutrals.5'>Showing Jobstatistic Jul 19-25</Text>
       </Stack>
 
-      <SegmentedControl data={['Week', 'Month', 'Year']} />
+      <SegmentedControl data={['Week', 'Month', 'Year']} size='md' />
     </Group>
   )
 }
