31fbece9f45ca1ed47afad3206b8766b0e65496e
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-13 12:30:05 +0330

    feat: add more on Jobs Statistics

--- src/components/job-huntly/components/company/dashboard/Home/components/JobStats/JobStats.tsx
+++ src/components/job-huntly/components/company/dashboard/Home/components/JobStats/JobStats.tsx
@@ -1,4 +1,17 @@
-import {Group, Paper, SegmentedControl, Stack, Text, Title} from '@mantine/core'
+'use client'
+
+import {
+  Grid,
+  Group,
+  Paper,
+  rem,
+  SegmentedControl,
+  Skeleton,
+  Stack,
+  Tabs,
+  Text,
+  Title,
+} from '@mantine/core'
 import React from 'react'
 
 function Header() {
@@ -14,11 +27,43 @@ function Header() {
   )
 }
 
+function StatsHeader() {
+  return (
+    <Tabs.List>
+      <Tabs.Tab value='overview'>Overview</Tabs.Tab>
+      <Tabs.Tab value='jobs-view'>Jobs View</Tabs.Tab>
+      <Tabs.Tab value='jobs-applied'>Jobs Applied</Tabs.Tab>
+    </Tabs.List>
+  )
+}
+
+function Stats() {
+  return (
+    <Tabs defaultValue='overview'>
+      <StatsHeader />
+
+      <Grid mt='xl'>
+        <Grid.Col span={8}>
+          <Skeleton h={rem(298)} />
+        </Grid.Col>
+
+        <Grid.Col span={4}>
+          <Stack h='100%'>
+            <Skeleton h='100%' />
+            <Skeleton h='100%' />
+          </Stack>
+        </Grid.Col>
+      </Grid>
+    </Tabs>
+  )
+}
+
 export function JobStats() {
   return (
     <Paper p='xl' withBorder>
       <Stack>
         <Header />
+        <Stats />
       </Stack>
     </Paper>
   )
