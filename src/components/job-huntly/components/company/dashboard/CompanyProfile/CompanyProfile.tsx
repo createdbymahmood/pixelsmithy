7a2ec47c2da7a8f4178fc4808c8d9a4a6953ea16
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-13 20:56:02 +0330

    feat: add company profile Contact

--- src/components/job-huntly/components/company/dashboard/CompanyProfile/CompanyProfile.tsx
+++ src/components/job-huntly/components/company/dashboard/CompanyProfile/CompanyProfile.tsx
@@ -28,6 +28,7 @@ import {head} from 'lodash-es'
 import React from 'react'
 
 import {EditButton} from '@/components/job-huntly/components/common'
+import {CompanyProfileSection} from '@/components/job-huntly/components/company/dashboard/CompanyProfile/common'
 import {companies} from '@/components/job-huntly/mock/companies'
 
 const company = head(companies)!
@@ -165,7 +166,23 @@ function Profile() {
 }
 
 function Contact() {
-  return <div>Contact</div>
+  return (
+    <CompanyProfileSection action={<EditButton />} title='Contact'>
+      <Text c='neutrals.5'>
+        Nomad is a software platform for starting and running internet
+        businesses. Millions of businesses rely on Stripe’s software tools to
+        accept payments, expand globally, and manage their businesses online.
+        Stripe has been at the forefront of expanding internet commerce,
+        powering new business models, and supporting the latest platforms, from
+        marketplaces to mobile commerce sites. We believe that growing the GDP
+        of the internet is a problem rooted in code and design, not finance.
+        Stripe is built for developers, makers, and creators. We work on solving
+        the hard technical problems necessary to build global economic
+        infrastructure—from designing highly reliable systems to developing
+        advanced machine learning algorithms to prevent fraud.
+      </Text>
+    </CompanyProfileSection>
+  )
 }
 
 function Gallery() {
