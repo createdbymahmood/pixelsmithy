06d938639cb4872588e098e8c4aacb3fa0a8d39c
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-13 11:41:41 +0330

    feat: add home page stat cards

--- src/components/job-huntly/components/company/dashboard/Home/Home.tsx
+++ src/components/job-huntly/components/company/dashboard/Home/Home.tsx
@@ -1,12 +1,13 @@
 import {Stack} from '@mantine/core'
 import React from 'react'
 
-import {Header} from './components'
+import {Header, Stats} from './components'
 
 export function Home() {
   return (
     <Stack gap='xxxl'>
       <Header />
+      <Stats />
     </Stack>
   )
 }
