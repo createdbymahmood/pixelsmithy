c813669ef6ecd93aad6c944dd8f4a8ac452375c1
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-14 13:52:58 +0330

    feat: add AllApplicants header

---
+++ src/components/job-huntly/components/company/dashboard/AllApplicants/components/Header/Header.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import {
+  Box,
+  Button,
+  Divider,
+  Group,
+  SegmentedControl,
+  TextInput,
+  Title,
+} from '@mantine/core'
+import {FunnelSimple, MagnifyingGlass} from '@phosphor-icons/react/dist/ssr'
+import React from 'react'
+
+function Search() {
+  const leftSection = (
+    <Box className='icon-size-lg' component={MagnifyingGlass} weight='bold' />
+  )
+
+  return (
+    <TextInput
+      leftSection={leftSection}
+      ml='auto'
+      placeholder='Search product name'
+      size='md'
+      variant='default'
+    />
+  )
+}
+
+function Filter() {
+  const leftSection = (
+    <Box className='icon-size-lg' component={FunnelSimple} weight='bold' />
+  )
+  return (
+    <Button fw='500' leftSection={leftSection} size='md' variant='default'>
+      Filter
+    </Button>
+  )
+}
+
+function ViewSwitch() {
+  return (
+    <SegmentedControl
+      data={['Pipeline View', 'Table View']}
+      defaultValue='Table View'
+      size='md'
+    />
+  )
+}
+
+export function Header() {
+  return (
+    <Group>
+      <Title order={4}>Total Applicants: 19</Title>
+      <Search />
+      <Filter />
+      <Divider my='xs' orientation='vertical' />
+      <ViewSwitch />
+    </Group>
+  )
+}
