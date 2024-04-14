aed7bbc6436b1cf1e8f7870d66d1e3575921b23f
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-15 01:26:15 +0330

    feat: add InterviewSchedule Header

--- src/components/job-huntly/components/company/dashboard/ApplicantDetails/InterviewSchedule/InterviewSchedule.tsx
+++ src/components/job-huntly/components/company/dashboard/ApplicantDetails/InterviewSchedule/InterviewSchedule.tsx
@@ -1,5 +1,39 @@
+'use client'
+
+import {Box, Button, Group, Stack, Text} from '@mantine/core'
+import {Plus} from '@phosphor-icons/react/dist/ssr'
 import React from 'react'
 
+function HeaderAction() {
+  const leftSection = (
+    <Box className='icon-size-md' component={Plus} weight='bold' />
+  )
+  return (
+    <Button
+      leftSection={leftSection}
+      ml='auto'
+      pr={0}
+      size='md'
+      variant='transparent'
+    >
+      Add Schedule Interview
+    </Button>
+  )
+}
+
+function Header() {
+  return (
+    <Group justify='space-between'>
+      <Text fw='600'>Interview List</Text>
+      <HeaderAction />
+    </Group>
+  )
+}
+
 export function InterviewSchedule() {
-  return <div>InterviewSchedule</div>
+  return (
+    <Stack>
+      <Header />
+    </Stack>
+  )
 }
