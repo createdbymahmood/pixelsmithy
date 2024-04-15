a9dff6bd1aa2e942b20f880067cedf7727ba5a56
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-15 12:43:28 +0330

    feat: apply applicants view switch

---
+++ src/components/job-huntly/components/company/dashboard/Applicants/components/ApplicantsPipeline/ApplicantsPipeline.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function ApplicantsPipeline() {
+  return <div>ApplicantsPipeline</div>
+}
--- src/components/job-huntly/components/company/dashboard/Applicants/components/index.ts
+++ src/components/job-huntly/components/company/dashboard/Applicants/components/index.ts
@@ -1,2 +1,3 @@
+export * from './ApplicantsPipeline'
 export * from './ApplicantsTable'
 export * from './Header'
