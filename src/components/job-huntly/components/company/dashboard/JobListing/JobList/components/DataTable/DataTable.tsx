0e7483335b728f3c724b2e9636933f93f7e9380f
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-15 11:08:49 +0330

    feat: add job list header

--- src/components/job-huntly/components/company/dashboard/JobListing/JobList/components/DataTable/DataTable.tsx
+++ src/components/job-huntly/components/company/dashboard/JobListing/JobList/components/DataTable/DataTable.tsx
@@ -1,19 +1,21 @@
 'use client'
 
+import {Paper, Stack} from '@mantine/core'
 import {DataTable as MantineDataTable} from 'mantine-datatable'
 import React from 'react'
 
 import {jobs} from '@/components/job-huntly/mock/jobs'
 
 import {columns} from './columns'
+import {Header} from './Header'
 
 export function DataTable() {
   return (
-    <MantineDataTable
-      columns={columns}
-      records={jobs}
-      striped
-      withTableBorder
-    />
+    <Stack gap={0}>
+      <Paper withBorder>
+        <Header />
+        <MantineDataTable columns={columns} records={jobs} striped />
+      </Paper>
+    </Stack>
   )
 }
