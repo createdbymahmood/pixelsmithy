be6597aaa7fe1a8c1fbd38a9aa6734a730b88983
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-16 14:46:42 +0330

    feat: add post a job href to company header

--- src/components/job-huntly/layout/CompanyDashboardPageLayout/components/Header/Header.tsx
+++ src/components/job-huntly/layout/CompanyDashboardPageLayout/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import {Box, Button, Group, rem} from '@mantine/core'
 import {Plus} from '@phosphor-icons/react/dist/ssr'
 import clsx from 'clsx'
+import Link from 'next/link'
 import React from 'react'
 
 import {Notification} from '@/components/job-huntly/layout/ApplicantDashboardPageLayout/components'
+import {urls} from '@/constants'
 
 import {CompanySwitch} from '../CompanySwitch'
 import styles from './Header.module.scss'
@@ -11,6 +13,9 @@ import styles from './Header.module.scss'
 export const COMPANY_DASHBOARD_PAGE_LAYAOUT_HEADER_HEIGHT = rem(90.59)
 
 export function Header() {
+  const postAJobLeftSection = (
+    <Box className='icon-size-md' component={Plus} weight='bold' />
+  )
   return (
     <Group
       bg='white'
@@ -27,9 +32,9 @@ export function Header() {
       </Box>
 
       <Button
-        leftSection={
-          <Box className='icon-size-md' component={Plus} weight='bold' />
-        }
+        component={Link}
+        href={urls.JobHuntly.company.dashboard.jobs.create}
+        leftSection={postAJobLeftSection}
         size='md'
       >
         Post a job
