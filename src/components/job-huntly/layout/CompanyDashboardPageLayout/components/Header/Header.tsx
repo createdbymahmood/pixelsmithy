c5709500bb7cc4ce5afffe9a60436a0b97038293
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-16 14:55:33 +0330

    feat: add callbackUrl pass to header post-a-job button

--- src/components/job-huntly/layout/CompanyDashboardPageLayout/components/Header/Header.tsx
+++ src/components/job-huntly/layout/CompanyDashboardPageLayout/components/Header/Header.tsx
@@ -1,11 +1,14 @@
+'use client'
 import {Box, Button, Group, rem} from '@mantine/core'
 import {Plus} from '@phosphor-icons/react/dist/ssr'
 import clsx from 'clsx'
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 import React from 'react'
+import urlcat from 'urlcat'
 
 import {Notification} from '@/components/job-huntly/layout/ApplicantDashboardPageLayout/components'
-import {urls} from '@/constants'
+import {CALLBACK_URL_PARAM_KEY, urls} from '@/constants'
 
 import {CompanySwitch} from '../CompanySwitch'
 import styles from './Header.module.scss'
@@ -16,6 +19,8 @@ export function Header() {
   const postAJobLeftSection = (
     <Box className='icon-size-md' component={Plus} weight='bold' />
   )
+  const callbackUrl = usePathname()
+  const disabled = callbackUrl === urls.JobHuntly.company.dashboard.jobs.create
   return (
     <Group
       bg='white'
@@ -33,7 +38,10 @@ export function Header() {
 
       <Button
         component={Link}
-        href={urls.JobHuntly.company.dashboard.jobs.create}
+        disabled={disabled}
+        href={urlcat(urls.JobHuntly.company.dashboard.jobs.create, {
+          [CALLBACK_URL_PARAM_KEY]: callbackUrl,
+        })}
         leftSection={postAJobLeftSection}
         size='md'
       >
