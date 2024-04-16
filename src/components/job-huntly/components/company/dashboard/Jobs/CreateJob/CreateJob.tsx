40be86c67f43d3ce016cea73d4b015566dba5202
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-16 14:58:42 +0330

    feat: add post a job header

--- src/components/job-huntly/components/company/dashboard/Jobs/CreateJob/CreateJob.tsx
+++ src/components/job-huntly/components/company/dashboard/Jobs/CreateJob/CreateJob.tsx
@@ -1,5 +1,41 @@
+'use client'
+
+import {Box, Group, rem, Stack, Title} from '@mantine/core'
+import {ArrowLeft} from '@phosphor-icons/react/dist/ssr'
+import Link from 'next/dist/client/link'
 import React from 'react'
 
+import {urls} from '@/constants'
+import {useCallbackUrl} from '@/hooks/useCallbackUrl'
+
+function Header() {
+  const [callbackUrl] = useCallbackUrl({
+    defaultValue: urls.JobHuntly.company.dashboard.jobs.listing,
+  })
+  const backArrow = (
+    <Link href={callbackUrl}>
+      <Box
+        className='cursor-pointer'
+        component={ArrowLeft}
+        h={rem(40)}
+        w={rem(40)}
+        weight='bold'
+      />
+    </Link>
+  )
+
+  return (
+    <Group>
+      {backArrow}
+      <Title order={4}>Post a Job</Title>
+    </Group>
+  )
+}
+
 export function CreateJob() {
-  return <div>CreateJob</div>
+  return (
+    <Stack>
+      <Header />
+    </Stack>
+  )
 }
