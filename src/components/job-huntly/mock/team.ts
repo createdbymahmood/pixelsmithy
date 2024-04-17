fbc308d48d4939e5c7f076743238a01c1e40bab4
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-17 12:07:01 +0330

    feat: add team settings

---
+++ src/components/job-huntly/mock/team.ts
@@ -0,0 +1,19 @@
+export interface TeamMember {
+  name: string
+  role: string
+}
+
+export const team: TeamMember[] = [
+  {
+    name: 'Célestin Gardinier',
+    role: 'CEO & Co-Founder',
+  },
+  {
+    name: 'Reynaud Colbert',
+    role: 'Co-Founder',
+  },
+  {
+    name: 'Arienne Lyon',
+    role: 'Managing Director',
+  },
+]
