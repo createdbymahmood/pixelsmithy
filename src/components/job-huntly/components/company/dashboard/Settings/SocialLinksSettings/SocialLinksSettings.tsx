8f4cc8e49e0410a2e0b6fe6425beb711b1aa0865
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-17 11:47:40 +0330

    feat: add social link settings

--- src/components/job-huntly/components/company/dashboard/Settings/SocialLinksSettings/SocialLinksSettings.tsx
+++ src/components/job-huntly/components/company/dashboard/Settings/SocialLinksSettings/SocialLinksSettings.tsx
@@ -1,5 +1,49 @@
+import {Stack, TextInput} from '@mantine/core'
 import React from 'react'
 
+import {FormSection} from '@/components/job-huntly/components/common/form'
+
+function FormView() {
+  return (
+    <FormSection
+      description='Add elsewhere links to your company profile. You can add only username without full https links.'
+      title='Basic Information'
+    >
+      <Stack gap='xl'>
+        <TextInput
+          label='Instagram'
+          placeholder='eg: instagram.com/createdbymahmood'
+          size='md'
+        />
+        <TextInput
+          label='Facebook'
+          placeholder='eg: fb.com/createdbymahmood'
+          size='md'
+        />
+        <TextInput
+          label='Twitter'
+          placeholder='eg: twitter.com/madebymahmood'
+          size='md'
+        />
+        <TextInput
+          label='Linkedin'
+          placeholder='eg: linkedin.com/in/createdbymahmood'
+          size='md'
+        />
+        <TextInput
+          label='Youtube'
+          placeholder='eg: yt.com/createdbymahmood'
+          size='md'
+        />
+      </Stack>
+    </FormSection>
+  )
+}
+
 export function SocialLinksSettings() {
-  return <div>SocialLinksSettings</div>
+  return (
+    <Stack gap='xl'>
+      <FormView />
+    </Stack>
+  )
 }
