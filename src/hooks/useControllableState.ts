f85b2ac805b80c46c32ef87ef46ed1d7334479b2
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-20 23:47:35 +0330

    feat: add useControllableState

---
+++ src/hooks/useControllableState.ts
@@ -0,0 +1,66 @@
+// import {useCallbackRef} from '@radix-ui/react-use-callback-ref'
+import {useCallbackRef} from '@mantine/hooks'
+import {noop} from 'lodash-es'
+import * as React from 'react'
+
+interface UseControllableStateParams<T> {
+  prop?: T | undefined
+  defaultProp?: T | undefined
+  onChange?: (state: T) => void
+}
+
+type SetStateFn<T> = (prevState?: T) => T
+
+function useControllableState<T>({
+  defaultProp,
+  onChange = noop,
+  prop,
+}: UseControllableStateParams<T>) {
+  const [uncontrolledProp, setUncontrolledProp] = useUncontrolledState({
+    defaultProp,
+    onChange,
+  })
+  const isControlled = prop !== undefined
+  const value = isControlled ? prop : uncontrolledProp
+  const handleChange = useCallbackRef(onChange)
+
+  const setValue: React.Dispatch<React.SetStateAction<T | undefined>> =
+    React.useCallback(
+      (nextValue) => {
+        if (isControlled) {
+          const setter = nextValue as SetStateFn<T>
+          // eslint-disable-next-line @typescript-eslint/no-shadow
+          const value =
+            typeof nextValue === 'function' ? setter(prop) : nextValue
+
+          if (value !== prop) handleChange(value as T)
+        } else {
+          setUncontrolledProp(nextValue)
+        }
+      },
+      [isControlled, prop, setUncontrolledProp, handleChange],
+    )
+
+  return [value, setValue] as const
+}
+
+function useUncontrolledState<T>({
+  defaultProp,
+  onChange,
+}: Omit<UseControllableStateParams<T>, 'prop'>) {
+  const uncontrolledState = React.useState<T | undefined>(defaultProp)
+  const [value] = uncontrolledState
+  const prevValueRef = React.useRef(value)
+  const handleChange = useCallbackRef(onChange)
+
+  React.useEffect(() => {
+    if (prevValueRef.current !== value) {
+      handleChange(value as T)
+      prevValueRef.current = value
+    }
+  }, [value, prevValueRef, handleChange])
+
+  return uncontrolledState
+}
+
+export {useControllableState}
