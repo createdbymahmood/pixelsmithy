599eb470f4d989449fa7aebfb537b08859a1a910
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-21 00:22:04 +0330

    feat: add paddings to container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,13 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
       screens: {
         '2xl': '1400px',
       },
