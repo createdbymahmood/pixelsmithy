e516bc3ac13901b6c2a8ae0b625341981ff70b79
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-20 13:28:44 +0330

    feat: add container component

---
+++ src/components/job-location/components/ui/container.tsx
@@ -0,0 +1,29 @@
+import {cn} from '@job-location/utils/cn'
+import {Slot} from '@radix-ui/react-slot'
+import type {VariantProps} from 'class-variance-authority'
+import {cva} from 'class-variance-authority'
+import * as React from 'react'
+
+const containerVariants = cva('container')
+
+export interface ContainerProps
+  extends React.HtmlHTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof containerVariants> {
+  asChild?: boolean
+}
+
+const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
+  ({asChild = false, className, ...props}, ref) => {
+    const Comp = asChild ? Slot : 'div'
+    return (
+      <Comp
+        ref={ref}
+        className={cn(containerVariants(), className)}
+        {...props}
+      />
+    )
+  },
+)
+Container.displayName = 'Button'
+
+export {Container, containerVariants}
