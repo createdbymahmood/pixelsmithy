46ba773a28b942ef430d646ced49aad629fc2b80
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-21 00:10:30 +0330

    feat: render slider thumbs dynamically

--- src/components/job-location/components/ui/slider.tsx
+++ src/components/job-location/components/ui/slider.tsx
@@ -4,25 +4,45 @@ import {cn} from '@job-location/utils/cn'
 import * as SliderPrimitive from '@radix-ui/react-slider'
 import * as React from 'react'
 
+import {useControllableState} from '@/hooks/useControllableState'
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({className, ...props}, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      'relative flex w-full touch-none select-none items-center',
-      className,
-    )}
-    {...props}
-  >
-    <SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-secondary'>
-      <SliderPrimitive.Range className='absolute h-full bg-primary' />
-    </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className='block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50' />
-    <SliderPrimitive.Thumb className='block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50' />
-  </SliderPrimitive.Root>
-))
+>(({className, ...props}, ref) => {
+  const [values = []] = useControllableState({
+    prop: props.value,
+    defaultProp: props.defaultValue,
+    onChange: props.onValueChange,
+  })
+
+  const thumbs = values.map((value) => {
+    return (
+      <SliderPrimitive.Thumb
+        key={value}
+        className='block size-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
+      />
+    )
+  })
+
+  return (
+    <SliderPrimitive.Root
+      ref={ref}
+      className={cn(
+        'relative flex w-full touch-none select-none items-center',
+        className,
+      )}
+      {...props}
+    >
+      <SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-secondary'>
+        <SliderPrimitive.Range className='absolute h-full bg-primary' />
+      </SliderPrimitive.Track>
+
+      {thumbs}
+    </SliderPrimitive.Root>
+  )
+})
+
 Slider.displayName = SliderPrimitive.Root.displayName
 
 export {Slider}
