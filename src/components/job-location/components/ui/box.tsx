61510d9c614bd20b226f5c0b428a228b1c412448
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-21 00:24:11 +0330

    feat: remove variant from box

--- src/components/job-location/components/ui/box.tsx
+++ src/components/job-location/components/ui/box.tsx
@@ -1,23 +1,15 @@
 import {cn} from '@job-location/utils/cn'
 import {Slot} from '@radix-ui/react-slot'
-import type {VariantProps} from 'class-variance-authority'
-import {cva} from 'class-variance-authority'
 import * as React from 'react'
 
-const boxVariants = cva('')
-
-export interface BoxProps
-  extends React.HtmlHTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof boxVariants> {
+export interface BoxProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   asChild?: boolean
 }
 
 const Box = React.forwardRef<HTMLDivElement, BoxProps>(
   ({asChild = false, className, ...props}, ref) => {
     const Comp = asChild ? Slot : 'div'
-    return (
-      <Comp ref={ref} className={cn(boxVariants(), className)} {...props} />
-    )
+    return <Comp ref={ref} className={cn(className)} {...props} />
   },
 )
 
