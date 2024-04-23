9de462ee55ddc9157468f2493861c629ff11736c
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-23 19:50:13 +0330

    fix: use previous version of demo for now

--- src/components/job-location/JobLocation.tsx
+++ src/components/job-location/JobLocation.tsx
@@ -1,38 +1,40 @@
 'use client'
 
-import {JobFilters} from '@/components/job-location/components/compound'
+import {Box, Container, Grid, rem} from '@mantine/core'
 
-// import {JobFilters} from '@job-location/components/compound'
+import {JobFilters} from '@/components/job-location/components/JobFilters'
+import {JobLocationHeader} from '@/components/job-location/components/JobLocationHeader'
+import {JobsList} from '@/components/job-location/components/JobsList'
+import {JobsSiderbar} from '@/components/job-location/components/JobsSidebar'
+import {useMobileDevice} from '@/hooks/useMobileDevice'
 
-// import {JobFilters} from '@/components/job-location/components/JobFilters'
+function JobLocationContent() {
+  return (
+    <Container maw={rem(1920)} my='lg'>
+      <Grid gutter='xl'>
+        <Grid.Col maw={rem(280)} offset={{xs: 0, lg: 1}} span='auto' w={280}>
+          <JobFilters />
+        </Grid.Col>
 
-// function JobLocationContent() {
-//   return (
-//     <Container maw={rem(1920)} my='lg'>
-//       <Grid gutter='xl'>
-//         <Grid.Col maw={rem(280)} offset={{xs: 0, lg: 1}} span='auto' w={280}>
-//           <JobFilters />
-//         </Grid.Col>
-
-//         <Grid.Col span={{lg: 'auto'}}>
-//           <JobsList />
-//         </Grid.Col>
-//       </Grid>
-//     </Container>
-//   )
-// }
-
-// export default function JobLocation() {
-//   const isMobile = useMobileDevice()
-//   return (
-//     <Box pt={isMobile ? 0 : rem(80)}>
-//       <JobsSiderbar />
-//       <JobLocationHeader />
-//       <JobLocationContent />
-//     </Box>
-//   )
-// }
+        <Grid.Col span={{lg: 'auto'}}>
+          <JobsList />
+        </Grid.Col>
+      </Grid>
+    </Container>
+  )
+}
 
 export default function JobLocation() {
-  return <JobFilters />
+  const isMobile = useMobileDevice()
+  return (
+    <Box pt={isMobile ? 0 : rem(80)}>
+      <JobsSiderbar />
+      <JobLocationHeader />
+      <JobLocationContent />
+    </Box>
+  )
 }
+
+// export default function JobLocation() {
+//   return <JobFilters />
+// }
