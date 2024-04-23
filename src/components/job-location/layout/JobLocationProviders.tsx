fcf0e0a040d4d4c3e90e1e29b73191052aa0e57a
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-23 14:17:42 +0330

    feat: import previous job location using mantine  for now

--- src/components/job-location/layout/JobLocationProviders.tsx
+++ src/components/job-location/layout/JobLocationProviders.tsx
@@ -1,27 +1,54 @@
+// 'use client'
+
+// import '@job-location/lib/style/globals.css'
+
+// import {cn} from '@job-location/utils/cn'
+// import type {ReactNode} from 'react'
+
+// import {openSans} from '@/lib/styles/font/openSans'
+// import {poppins} from '@/lib/styles/font/poppins'
+
+// interface RootLayoutProps {
+//   children: ReactNode
+// }
+
+// export function JobLocationProviders({children}: RootLayoutProps) {
+//   return (
+//     <div
+//       className={cn(
+//         'min-h-screen bg-background font-sans antialiased',
+//         poppins.variable,
+//         openSans.variable,
+//       )}
+//     >
+//       {children}
+//     </div>
+//   )
+// }
+
 'use client'
 
-import '@job-location/lib/style/globals.css'
+import '@mantine/core/styles.css'
+import 'react-perfect-scrollbar/dist/css/styles.css'
+import '@/lib/styles/global.scss'
 
-import {cn} from '@job-location/utils/cn'
+import {MantineProvider} from '@mantine/core'
 import type {ReactNode} from 'react'
 
-import {openSans} from '@/lib/styles/font/openSans'
-import {poppins} from '@/lib/styles/font/poppins'
+import {JobLocationTheme} from '@/components/job-location/lib/style/JobLocationTheme'
+import {defaultThemeColorScheme} from '@/constants'
 
-interface RootLayoutProps {
+interface JobLocationProvidersProps {
   children: ReactNode
 }
 
-export function JobLocationProviders({children}: RootLayoutProps) {
+export function JobLocationProviders({children}: JobLocationProvidersProps) {
   return (
-    <div
-      className={cn(
-        'min-h-screen bg-background font-sans antialiased',
-        poppins.variable,
-        openSans.variable,
-      )}
+    <MantineProvider
+      defaultColorScheme={defaultThemeColorScheme}
+      theme={JobLocationTheme}
     >
       {children}
-    </div>
+    </MantineProvider>
   )
 }
