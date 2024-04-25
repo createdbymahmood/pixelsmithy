bbf26b5be99a5901862c69838cd31408c4f508ea
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-25 13:57:07 +0330

    feat: add navbar notifications

---
+++ src/components/job-location/components/compound/navbar/navbar-notifications.tsx
@@ -0,0 +1,12 @@
+import {Bell} from 'lucide-react'
+import React from 'react'
+
+import {Button} from '@/components/job-location/components/ui'
+
+export function NavbarNotifications() {
+  return (
+    <Button size='icon' variant='outline'>
+      <Bell className='text-primary' />
+    </Button>
+  )
+}
