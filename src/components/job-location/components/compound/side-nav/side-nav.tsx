40abc4eafd31bece20b41e26ccbfaf4e7d6ee8bc
Mahmood Bagheri <createdbymahmood@gmail.com>
2024-04-26 01:01:26 +0330

    fix: place sidenav 1/3 instead of 1/2

--- src/components/job-location/components/compound/side-nav/side-nav.tsx
+++ src/components/job-location/components/compound/side-nav/side-nav.tsx
@@ -7,7 +7,7 @@ import {SideNavItem} from './side-nav-item'
 export function SideNav() {
   return (
     <Stack
-      className='absolute left-0 top-1/2 -translate-y-1/2 rounded-e-4xl bg-white px-3 py-10'
+      className='absolute left-0 top-1/3 -translate-y-1/3 rounded-e-4xl bg-white px-3 py-10'
       gap='md'
     >
       <SideNavItem active>
